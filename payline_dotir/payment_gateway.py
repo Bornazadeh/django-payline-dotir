2ce6fd507b5e10ff09845ea3d1a65b9055f7e5f5
Mahdi Bornazadeh <Bornazadeh@gmail.com>
2015-06-27 22:53:23 +0430

    intial payline_dotir app

---
+++ payline_dotir/payment_gateway.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+import requests
+
+from django.utils.translation import ugettext as _
+
+from .settings import SEND_URL_FINAL, GATEWAY_URL_FINAL
+from .settings import CHECK_URL_FINAL, PAYLINE_DOTIR_API_FINAL
+
+def send_url(amount, redirect_url, url, api):
+    '''
+    return payment gateway url to redirect user
+    to it for payment.
+    '''
+    values = {'api': api, 'amount': amount, 'redirect': redirect_url}
+    send_request = requests.post( SEND_URL_FINAL, data = values)
+    id_get = send_request.text
+    print(id_get)
+    if int(id_get) > 0:
+        print(".معتبر است id_get")
+    elif id_get == "-1":
+        print("‫‪ api‬ارسالی با نوع ‪ api‬تعریف شده در ‪ payline‬سازگار نیست.‬")
+    elif id_get == "-2":
+        print("‫مقدار ‪ amount‬داده عددي نمی باشد و یا کمتر از 1000 ریال است.‬")
+    elif id_get == "-3":
+        print("‫مقدار ‪ redirect‬رشته ‪ null‬است.‬")
+    elif id_get == "-4":
+        print("‫درگاهی با اطلاعات ارسالی شما یافت نشده و یا در حالت انتظار می باشد.‬")
+    else:
+        print("some other error(s) occurred.")
+    payment_gateway_url = '%s%s' % (GATEWAY_URL_FINAL, id_get)
+
+    return payment_gateway_url
+
+
+def get_result(api, trans_id, id_get):
+    '''
+    Check if the given id_get and trans_id is from a valid transaction.
+    '''
+    check_values = {'api': api, 'id_get': id_get, 'trans_id': trans_id}
+
+    check_transaction = requests.post(CHECK_URL_FINAL, data= check_values)
+    result = check_transaction.text
+    if result == "1":
+        print(".‫تراکنش موفقیت آمیز بوده است‬")
+        return True
+    elif result == "-1":
+        print("‫‪ api‬ارسالی با نوع ‪ api‬تعریف شده در ‪ payline‬سازگار نیست.‬")
+    elif result == "-2":
+        print(".ارسال شده معتبر نمی باشد‬ trans_id")
+    elif result == "-3":
+        print(".‫id_get ارسال شده معتبر نمی باشد‬")
+    elif result == "-4":
+        print(".‫چنین تراکنشی در سیستم وجود ندارد و یا موفقیت آمیز نبوده است‬")
+    else:
+        print("some error(s) occurred, please try again.")
+
---
+++ payline_dotir/settings.py
@@ -0,0 +1,30 @@
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+
+
+SEND_URL = 'http://payline.ir/payment/gateway-send'
+SEND_URL_TEST = 'http://payline.ir/payment-test/gateway-send'
+#PAYLINE_DOTIR_API = settings.PAYLINE_DOTIR_API
+PAYLINE_DOTIR_API_TEST = 'adxcv-zzadq-polkjsad-opp13opoz-1sdf455aadzmck1244567'
+GATEWAY_URL = 'http://payline.ir/payment/gateway-'
+GATEWAY_URL_TEST = 'http://payline.ir/payment-test/gateway-'
+CHECK_URL = 'http://payline.ir/payment/gateway-result-second'
+CHECK_URL_TEST = 'http://payline.ir/payment-test/gateway-result-second'
+PAYLINE_DOTIR_TEST = settings.PAYLINE_DOTIR_TEST
+
+try:
+    PAYLINE_DOTIR_API = settings.PAYLINE_DOTIR_API
+except AttributeError:
+    raise ImproperlyConfigured("You need to define PAYLINE_DOTIR_API "
+                               "in your settings module "
+                               "to use the payline.ir payment processor.")
+if not PAYLINE_DOTIR_TEST:
+    SEND_URL_FINAL = SEND_URL
+    GATEWAY_URL_FINAL = GATEWAY_URL
+    CHECK_URL_FINAL = CHECK_URL
+    PAYLINE_DOTIR_API_FINAL = PAYLINE_DOTIR_API
+else:
+    SEND_URL_FINAL = SEND_URL_TEST
+    GATEWAY_URL_FINAL = GATEWAY_URL_TEST
+    CHECK_URL_FINAL = CHECK_URL_TEST
+    PAYLINE_DOTIR_API_FINAL = PAYLINE_DOTIR_API_TEST
